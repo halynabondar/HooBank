5b70501566518955b719b1395b0f86c998c08453
Halyna Bondar <galya.marusyak@gmail.com>
2025-01-07 15:01:24 +0100

    Change margin

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
     return (
         <nav className="w-full flex py-6 justify-between items-center navbar">
             <img src={logo} alt="hoobank" className="w-[124px] h-[32px] ml-10" />
-            <ul className="list-none sm:flex hidden justify-end items-center flex-1 mr-5">
+            <ul className="list-none sm:flex hidden justify-end items-center flex-1 mr-10">
                 {navLinks.map((nav, index) => (
                     <li key={nav.id} className={`font-poppins font-normal cursor-pointer text-[16px] ${active === nav.title ? "text-white" : "text-dimWhite"} ${index === navLinks.length - 1 ? "mr-0" : "mr-10"} text-white`}>
                         <a href={`#${nav.id}`}>
